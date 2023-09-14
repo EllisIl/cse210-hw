9392d428d7e9f94e56d21ef0854c575d0387ca63
Elijah Foard <efoard@byui.edu>
2023-09-14 14:20:47 -0600

    finished prove assignments 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+
+        int grade;
+        char letterGrade, gradeSign;
+
+        Console.Write("What was your grade? ");
+        grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90){
+            letterGrade = 'A';
+        }
+        else if (grade >= 80){
+            letterGrade = 'B';
+        }
+        else if (grade >= 70){
+            letterGrade = 'C';
+        }
+        else if (grade >= 60){
+            letterGrade = 'D';
+        }
+        else{
+            letterGrade = 'F';
+        }
+
+        int lastDigit = grade % 10;
+
+        if (letterGrade != 'F' && grade < 100){
+            if (lastDigit >= 7 && letterGrade != 'A'){
+                gradeSign = '+';
+            }
+            else if (lastDigit < 3){
+                gradeSign = '-';
+            }
+            else{
+                gradeSign = ' ';
+            }
+        } else {
+            gradeSign = ' ';
+        }
+            
+        Console.WriteLine("Your letter grade is " + letterGrade + gradeSign);
     }
 }
