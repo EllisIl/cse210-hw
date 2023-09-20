2535f971fc5048dd6732ff7b10234261427ab91f
Elijah Foard <efoard@byui.edu>
2023-09-20 11:31:32 -0600

    added prep 5 program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string UserName = PromptUserName();
+        int FavNum = PromptUserNumber();
+        int SqNum = SquareNumber(FavNum);
+        DisplayResult(UserName, SqNum);
+    }
+
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName(){
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber(){
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int num){
+        return num * num;
+    }
+
+    static void DisplayResult(string UserName, int SqNum){
+        Console.WriteLine($"{UserName}, the square of your number is {SqNum}");
     }
 }
