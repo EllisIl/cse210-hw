5ee4e20d40769ec3282ceffca763b1f84c5edc86
Elijah Foard <efoard@byui.edu>
2023-09-26 16:26:32 -0600

    finished Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job
+        {
+            _startYear = 2014,
+            _endYear = 2017,
+
+            _company = "Microsoft",
+            _jobTitle = "Software Developer"
+        };
+
+        Job job2 = new Job
+        {
+            _startYear = 2017,
+            _endYear = 2024,
+
+            _company = "Apple",
+            _jobTitle = "Manager"
+        };
+
+        Resume myResume = new Resume
+        {
+            _name = "Elijah Foard"
+        };
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
